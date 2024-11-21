d28cf662d8f20e5a39f138fe9b8dc54cb9c2190c
sayedkhaledd <sayedkhaledsa@gmail.com>
2024-11-21 16:01:39 +0200

    init structure

---
+++ company-country-assignment-app/src/main/java/com/example/companycountryassignmentapp/CompanyCountryAssignmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.companycountryassignmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyCountryAssignmentAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyCountryAssignmentAppApplication.class, args);
+    }
+
+}
---
+++ company-country-assignment-app/src/test/java/com/example/companycountryassignmentapp/CompanyCountryAssignmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.companycountryassignmentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyCountryAssignmentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
